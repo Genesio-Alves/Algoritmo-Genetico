f7b78f6045fa92c36e3ca5316724e39b3303e5e7
GenesisXXXx <58094748+GenesisXXXx@users.noreply.github.com>
2019-11-23 23:21:47 -0300

    Add files via upload

---
+++ Estatistica.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Algoritmo_genettico
+{
+    
+        class Estatistica
+        {
+            public int MelhorIndividuo;
+            public int PiorIndividuo;
+            public double AptidaoMelhorIndividuo;
+            public double AptidaoPiorIndividuo;
+
+            int i;
+
+            public void GraficoAptidao(int N, Individuo[] individuo)
+            {
+
+                double[] x = new double[N];
+                for (i = 0; i < N; i++)
+                {
+                    x[i] = -1 + (3 * individuo[i].genes / 1000);
+                }
+
+            }
+
+
+        }
+    }
+
